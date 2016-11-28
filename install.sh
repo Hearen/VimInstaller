6c9ce491b5ff3ac1f8ec926a4d9c324e5bc4db60
Hearen <LHearen@126.com>
2016-11-28 10:55:20 +0800

    original

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#-------------------------------------------
+# Author      : LHearen
+# E-mail      : LHearen@126.com
+# Time        : 2016-11-28 10:45
+# Description : Install latest Vim and configure
+#   it for basic personal use
+#-------------------------------------------
+
+http://www.fullybaked.co.uk/articles/installing-latest-vim-on-centos-from-source
