2feaf664fbaeb6f0b7b802dfdb0bc21b7f2bc007
Hearen <LHearen@126.com>
2016-11-28 11:22:23 +0800

    avoid duplicates

--- install.sh
+++ install.sh
@@ -12,7 +12,8 @@
 echo "Copy vimrc..."
 cp -r etc/vimrc $HOME/.vimrc
 echo "Copy colors..."
-cp -r etc/vim/* $HOME/.vim
+rm -rf $HOME/.vim/bundle
+cp -r etc/vim/colors $HOME/.vim/
 echo "Clone Vundle.vim..."
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 echo "Run BundleInstall in vim now!"
