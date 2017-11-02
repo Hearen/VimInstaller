297937f39e07913e9e5a15e0d03769c96efbc3c5
LHearen <lhearen@gmail.com>
2017-11-02 08:32:42 +0800

    color files can be properly copied - issued fixed

--- install.sh
+++ install.sh
@@ -14,7 +14,10 @@ cp -r etc/vimrc $HOME/.vimrc
 echo "Copy colors..."
 rm -rf $HOME/.vim/bundle
 mkdir -p $HOME/.vim/colors
-cp -r etc/vim/colors $HOME/.vim/colors
+for color in etc/vim/colors/*
+do
+    cp $color $HOME/.vim/colors
+done
 echo "Clone Vundle.vim..."
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 echo "Run BundleInstall in vim now!"
